62a3ad4577ef8150d2e84e7c4fbf96c1138bb2f5
estebancb03 <estebanr.castaneda03@gmail.com>
2022-02-23 01:30:49 -0600

    View routers

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,12 @@ import express from 'express';
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('home');
+});
+
+router.get('/we', (req, res) => {
+    res.render('we');
+});
+
 export default router;
