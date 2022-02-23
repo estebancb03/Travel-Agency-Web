01fa737566ab1fb8fb76b229052d2a3465879786
estebancb03 <estebanr.castaneda03@gmail.com>
2022-02-23 00:45:41 -0600

    Router was created and PUG was installed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import router from './routes/index.js';
+
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use('/', router);
 app.listen(port, () => {
     console.log(`Server running on port ${ port }`);
 });
