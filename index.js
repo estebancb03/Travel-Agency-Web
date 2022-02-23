c6871037ca10d0bfae7c1f450303ef83883463fd
estebancb03 <estebanr.castaneda03@gmail.com>
2022-02-23 01:31:30 -0600

    PUG was enabled

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import router from './routes/index.js';
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.set('view engine', 'pug');
 app.use('/', router);
 app.listen(port, () => {
     console.log(`Server running on port ${ port }`);
