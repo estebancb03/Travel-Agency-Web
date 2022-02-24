ab15f079afabd86f7c26f9c1e96c2ca083591092
estebancb03 <estebanr.castaneda03@gmail.com>
2022-02-23 23:54:31 -0600

    Progress in views and rendering

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.set('view engine', 'pug');
+
+app.use((req, res, next) => {
+    const year = new Date();
+    res.locals.currentYear = year.getFullYear();
+    res.locals.webName = 'Agencia de viajes';
+    next();
+});
+
 app.use('/', router);
+app.use(express.static('public'));
+
 app.listen(port, () => {
     console.log(`Server running on port ${ port }`);
 });
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,27 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        page: 'Inicio'
+    });
 });
 
 router.get('/we', (req, res) => {
-    res.render('we');
+    res.render('we', {
+        page: 'Nosotros'
+    });
+});
+
+router.get('/travels', (req, res) => {
+    res.render('travels', {
+        page: 'Viajes'
+    });
+});
+
+router.get('/testimonials', (req, res) => {
+    res.render('testimonials', {
+        page: 'Testimonios'
+    });
 });
 
 export default router;
