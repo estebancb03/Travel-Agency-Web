c5ec0cdaa2c85ed57d4f94b51677f06fc6f4549c
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-02 21:53:57 -0600

    Route of the more information of each trip page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { homePage, wePage, travelsPage, testimonialsPage } from '../controllers/pagesController.js'
+import { 
+    homePage, 
+    wePage, 
+    travelsPage, 
+    tripDetailsPage, 
+    testimonialsPage 
+} from '../controllers/pagesController.js'
 
 const router = express.Router();
 router.get('/', homePage);
 router.get('/we', wePage);
 router.get('/travels', travelsPage);
+router.get('/travels/:slug', tripDetailsPage);
 router.get('/testimonials', testimonialsPage);
 
 export default router;
