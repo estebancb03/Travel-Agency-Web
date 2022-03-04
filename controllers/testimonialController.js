045102648278af20f8eadc3b33236557338679ec
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-04 01:19:39 -0600

    Testimonial's controller created

---
+++ controllers/testimonialController.js
@@ -0,0 +1,35 @@
+import { Testimonial } from "../models/Testimonial.js";
+
+const saveTestimonial = async (req, res) => {
+    const { name, email, message } = req.body;
+    const errors = [];
+    if(name.trim() === '') 
+        errors.push({ message: 'Name is empty' });
+    if(email.trim() === '') 
+        errors.push({ message: 'Email is empty' });
+    if(message.trim() === '') 
+        errors.push({ message: 'Message is empty' });
+    if(errors.length > 0) {
+        res.render('testimonials', {
+            page: 'Testimonials',
+            errors,
+            name,
+            email,
+            message
+        });
+    } 
+    else {
+        try {
+            await Testimonial.create({
+                name,
+                email, 
+                message
+            });
+            res.redirect('./testimonials');
+        } catch(error) {
+            console.error(error);
+        }
+    }
+}
+
+export { saveTestimonial }
