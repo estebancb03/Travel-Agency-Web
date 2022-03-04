adeba805e15f163928b1721fdc8673e135f64f9c
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-04 01:21:17 -0600

    Route for testimonial form created

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import { saveTestimonial } from '../controllers/testimonialController.js';
 import { 
     homePage, 
     wePage, 
     travelsPage, 
     tripDetailsPage, 
     testimonialsPage 
-} from '../controllers/pagesController.js'
+} from '../controllers/pagesController.js';
 
 const router = express.Router();
 router.get('/', homePage);
@@ -13,5 +14,6 @@ router.get('/we', wePage);
 router.get('/travels', travelsPage);
 router.get('/travels/:slug', tripDetailsPage);
 router.get('/testimonials', testimonialsPage);
+router.post('/testimonials', saveTestimonial);
 
 export default router;
