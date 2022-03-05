a3f6b19d5aa8ec7b710e5248c82c9a40be63e3dc
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-05 01:15:12 -0600

    Dependencies and modifcations necessary for deployment in production were added

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
 
-const db = new Sequelize('travelsagency', 'root', 'ercb03042001', {
-    host: '127.0.0.1',
-    port: '3306',
+dotenv.config({ path: 'variables.env' });
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define: { 
         timestamps: false 
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import router from './routes/index.js';
 import db from './config/db.js'
+import dotenv from 'dotenv';
 
+dotenv.config({ path: 'variables.env' });
 const app = express();
 
 db.authenticate()
@@ -9,13 +11,14 @@ db.authenticate()
     .catch(error => console.error(error));
 
 const port = process.env.PORT || 4000;
+const host = process.env.HOST || '0.0.0.0';
 
 app.set('view engine', 'pug');
 
 app.use((req, res, next) => {
     const year = new Date();
     res.locals.currentYear = year.getFullYear();
-    res.locals.webName = 'Agencia de viajes';
+    res.locals.webName = 'Travel Agency';
     next();
 });
 
@@ -24,6 +27,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
 app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log(`Server running on port ${ port }`);
+app.listen(port, host, () => {
+    console.log(`Server running on port ${ port } and host ${ host }`);
 });
